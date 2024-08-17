f87804af77ff7e984b042d3d80e56fb81b73896c
Hareesh123NH <nagasarihareesh2005@gmail.com>
2024-08-17 20:29:10 +0530

    contest done

--- script.js
+++ script.js
@@ -8,20 +8,42 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map(employee=> {
+        if(employee.profession==="developer"){
+            console.log(employee);
+            }
+     });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+   arr.forEach(employee=> {
+        if(employee.profession==="developer"){
+            console.log(employee);
+            }
+     });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newEmployee={id:4,name:"Hari",age:"20",profession:"intern"};
+  arr.push(newEmployee);
+  console.log(arr);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr=arr.filter(employee=>employee.profession!=="admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newEmployees = [
+          {id: 5, name: "charan", age: "22", profession: "developer"},
+          {id: 6, name: "manogna", age: "21", profession: "designer"},
+          {id: 7, name: "sarvika", age: "23", profession: "manager"}
+      ];
+      let combinedArray = arr.concat(newEmployees);
+      console.log(combinedArray);
 }
